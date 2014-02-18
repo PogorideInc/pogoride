ce9d1bc48c5fea873763497a65632a532369baae
switters <codenomadic@gmail.com>
2014-02-18 21:19:33 +0000

    Basic user views up, along with current_user and some permission-based redirection.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+
+  def current_user
+  	@current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def if_not_user_redirect
+  	if current_user
+    	unless current_user.id
+    	  redirect_to root_path, notice: "You need to sign up to access these features."
+    	end
+    else 
+    	redirect_to root_path
+    end
+  end
+
 end
