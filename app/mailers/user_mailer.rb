1a3a4e766432e973640527ec64548e0b4126c5f8
switters <codenomadic@gmail.com>
2014-02-22 00:37:19 +0000

    Message to driver on new passenger request has material.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,16 @@
 class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "pogoridelhl@gmail.com"
+
+  def welcome_email
+
+  end
+
+  def new_requested_passenger(driver, new_passenger, ride)
+  	@driver = driver
+  	@new_passenger = new_passenger
+  	@ride = ride
+  	mail(to: @driver.email, subject: "#{@new_passenger.username} wants to join your ride from #{@ride.from} to #{@ride.to}.")
+  end
+
+
 end
