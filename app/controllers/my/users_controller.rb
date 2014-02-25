174c03f765879e114e600c40156e53a17758f9bb
Ali Yazdani <aliyazdani82@gmail.com>
2014-02-25 20:36:24 +0000

    permitted avatar in user_params

--- app/controllers/my/users_controller.rb
+++ app/controllers/my/users_controller.rb
@@ -2,9 +2,6 @@
 class My::UsersController < My::MyController
 
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
-
   def show
     @user = User.where(id: params[:id]).first
     @rides = @user.rides.where(user_id: @user.id)
@@ -35,6 +32,7 @@ class My::UsersController < My::MyController
       :about_me, 
       :password, 
       :password_confirmation
+      :avatar
       )
   end
 
