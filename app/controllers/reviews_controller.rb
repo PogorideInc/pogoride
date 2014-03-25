afc40e69461d1cf26d0c915c8475d87f7851a556
Ian Smith <iansmith9876@gmail.com>
2014-03-25 18:35:53 +0000

    can create reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,15 +1,16 @@
 class ReviewsController < ApplicationController
 
   def new
-    @user = User.find(params[:for_user_id])
+    @user = User.find(params[:user_id])
     @review = @user.reviews.build
   end
 
   def create
-    @user = User.find(params[:for_user_id])
+    @user = User.find(params[:user_id])
     @review = @user.reviews.build(review_params)
     @review.user_id = current_user.id
-
+    @review.for_user_id = @user.id
+    @review.author = current_user.username
     if @review.save
       redirect_to @user, notice: "Review created successfully"
     else
@@ -20,7 +21,7 @@ class ReviewsController < ApplicationController
   protected
 
   def review_params
-    params.require(:review).permit(:text, :rating_out_of_ten)
+    params.require(:review).permit(:content, :rating)
   end
 
 end
